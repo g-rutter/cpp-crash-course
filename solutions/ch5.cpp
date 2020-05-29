217852a92db3b2d0cdd63de2ce1a6cd77f5dc80f
Gil Rutter <g.o.rutter@gmail.com>
2020-05-29 22:29:55 +0100

    Remove useless const keywords, but add some useful ones

--- solutions/ch5.cpp
+++ solutions/ch5.cpp
@@ -13,12 +13,12 @@
 struct Logger {
     Logger() = default;
     virtual ~Logger() = default;
-    virtual const void log_transfer(const long from, const long to, const double amount) = 0;
+    virtual void log_transfer(const long from, const long to, const double amount) const = 0;
 };
 
 struct ConsoleLogger : Logger{
     ConsoleLogger(const char* name) : name{name} {}
-    const void log_transfer(const long from, const long to, const double amount) override{
+    void log_transfer(const long from, const long to, const double amount) const override{
         printf("[%s] %ld -> %ld: %f\n", name, from, to, amount);
     }
     protected:
@@ -27,42 +27,44 @@ struct ConsoleLogger : Logger{
 
 struct AccountDatabase {
     virtual ~AccountDatabase() = default;
-    virtual const double get_balance(const long account_id) = 0;
-    virtual const void set_balance(const long account_id, double amount) = 0;
+    virtual double get_balance(const long account_id) = 0;
+    virtual void set_balance(const long account_id, double amount) = 0;
 };
 
 struct InMemoryAccountDatabase : AccountDatabase {
     std::unordered_map<long, double> balances;
 
-    const double get_balance(const long account_id) override {
+    double get_balance(const long account_id) override {
         return balances[account_id];
     }
 
-    const void set_balance(const long account_id, const double amount) override {
+    void set_balance(const long account_id, const double amount) override {
         balances[account_id] = amount;
     }
 };
 
 struct Bank {
-    Bank(Logger* logger, AccountDatabase& account_db) : logger{ logger }, account_db{ account_db } {}
-    const void set_logger(Logger* new_logger) {
+    Bank(const Logger* const logger, AccountDatabase& account_db) : logger{ logger }, account_db{ account_db } {}
+    void set_logger(const Logger* const new_logger) {
         logger = new_logger;
     }
-    const void make_transfer(const long from, const long to, const double amount) {
+    void make_transfer(const long from, const long to, const double amount) const {
         account_db.set_balance(from, account_db.get_balance(from) - amount);
         account_db.set_balance(to, account_db.get_balance(to) + amount);
         if (logger) logger->log_transfer(from, to, amount);
     }
     private:
-        Logger* logger;
+        const Logger* logger;
         AccountDatabase& account_db;
 };
 
 int main() {
     auto logger{ConsoleLogger("hello")};
+    auto const logger2{ConsoleLogger("hi2")};
     auto account_db{InMemoryAccountDatabase()};
 
     auto bank{Bank(&logger, account_db)};
+    bank.set_logger(&logger2);
     bank.make_transfer(21L, 55L, 66.6);
 
     printf("%f", account_db.get_balance(21L));
