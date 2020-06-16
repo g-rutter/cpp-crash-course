60cf7dc78645011717e5081f02ac5b4637c3ae10
Gil Rutter <g.o.rutter@gmail.com>
2020-06-16 12:12:36 +0100

    Remove 14.3, not going to complete

--- solutions/ch14/3.cpp
+++
@@ -1,6 +0,0 @@
-// EXERCISES
-// 14-3. Write a program that uses the Stopwatch class in Listing 12-25 to determine
-//       the runtime performance of std::advance when given a forward iterator from
-//       a large std::forward_list and a large std::vector. How does the run-time
-//       change with the number of elements in the container? (Try hundreds of
-//       thousands or millions of elements.)
